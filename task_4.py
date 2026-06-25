7226a8183a265a3f9a5e14d923c5d159b915f310
dpotanin44-tech <dpotanin44@gmail.com>
2026-06-25 14:10:04 +0700

    Create task_4.py

---
+++ task_4.py
@@ -0,0 +1,19 @@
+def func_1():
+    x = 1
+    y = 2
+    return x + y
+
+
+def func_2(a, b):
+    if a > b:
+        return a
+    else:
+        return b
+
+
+class Test:
+    def method_1(self):
+        pass
+
+    def method_2(self):
+        pass
